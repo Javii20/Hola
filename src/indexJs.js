31d1c095fde5365cb8ecb4957f68c900d517a2cb
zoor <hector.llorca.7e3@itb.cat>
2020-03-24 17:37:37 +0100

    first js

--- src/indexJs.js
+++ src/indexJs.js
@@ -1,5 +1,15 @@
-function bodyChange() {
+   const player = document.querySelector('.videoDoom');
+   const imagen = document.querySelector(".juego");
 
 
 
+function removeMostrar(){
+    player.style.transition= " all 1s";
+    player.classList.remove('mostrar')
 }
+   function addMostrar(){
+
+       player.classList.add('mostrar')
+   }
+imagen.addEventListener('mouseover',removeMostrar)
+imagen.addEventListener('mouseout',addMostrar)
